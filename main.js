170216931bc711ddc2c9c73cef806b65ebbc46d9
liujiaqing <2664707780@qq.com>
2023-08-08 14:59:00 +0800

    feat: 解决读取不到环境变量的问题

--- main.js
+++ main.js
@@ -6,10 +6,14 @@ const escodegen = require('escodegen');
 const { dialog } = require('electron');
 
 const getPlayerPath = () => {
-  if (process.env.NODE_ENV === 'production') {
+  try {
+    if (process.env.NODE_ENV === 'development') {
+      return path.join(__dirname, 'player');
+    }
     return path.join(__dirname, '../player');
+  } catch (e) {
+    dialog.showErrorBox('error fn getPlayerPath', `${JSON.stringify(e.message)}`);
   }
-  return path.join(__dirname, 'player');
 };
 
 const getPlayerFileName = (directoryPath) => {
@@ -21,39 +25,47 @@ const getPlayerFileName = (directoryPath) => {
         return path.join(directoryPath, item);
       })
       .slice(0, 2);
-  } catch (err) {
-    console.error('Error reading directory:', err);
+  } catch (e) {
+    dialog.showErrorBox('error fn getPlayerFileName', `${JSON.stringify(e.message)} args: ${directoryPath}`);
   }
 };
 
 const getRun = (filePath) => {
-  const scriptContent = fs.readFileSync(filePath, 'utf-8');
-  const ast = acorn.parse(scriptContent, { sourceType: 'module' });
-  const runAstNode = ast?.body?.find((node) => {
-    if (node.type === 'FunctionDeclaration' && node.id.name === 'run') {
-      return true;
+  try {
+    const scriptContent = fs.readFileSync(filePath, 'utf-8');
+    const ast = acorn.parse(scriptContent, { sourceType: 'module' });
+    const runAstNode = ast?.body?.find((node) => {
+      if (node.type === 'FunctionDeclaration' && node.id.name === 'run') {
+        return true;
+      }
+    });
+    if (!runAstNode) {
+      dialog.showErrorBox('错误的脚本', `未在路径${filePath}下找到run方法`);
     }
-  });
-  if (!runAstNode) {
-    dialog.showErrorBox('错误的脚本', `未在路径${filePath}下找到run方法`);
+    const script = new Function(`return ${escodegen.generate(runAstNode)}`)();
+    return script;
+  } catch (e) {
+    dialog.showErrorBox('error fn getRun', `${JSON.stringify(e.message)}`);
   }
-  const script = new Function(`return ${escodegen.generate(runAstNode)}`)();
-  return script;
 };
 
 const getScipts = () => {
-  // 读取 JavaScript 脚本文件
-  const playerDir = getPlayerPath();
-  const players = getPlayerFileName(playerDir);
+  try {
+    // 读取 JavaScript 脚本文件
+    const playerDir = getPlayerPath();
+    const players = getPlayerFileName(playerDir);
 
-  if (players?.length < 2) {
-    dialog.showErrorBox('目标目录下需要两个可执行脚本', `${playerDir}`);
+    if (players?.length < 2) {
+      dialog.showErrorBox('目标目录下需要两个可执行脚本', `${playerDir}`);
+    }
+
+    const script1 = getRun(players?.[0]);
+    const script2 = getRun(players?.[1]);
+
+    return { script1, script2 };
+  } catch (e) {
+    dialog.showErrorBox('error fn getScipts', `${JSON.stringify(e.message)}`);
   }
-
-  const script1 = getRun(players?.[0]);
-  const script2 = getRun(players?.[1]);
-
-  return { script1, script2 };
 };
 
 const createWindow = () => {
@@ -71,18 +83,22 @@ const createWindow = () => {
     },
   });
 
-  const { script1, script2 } = getScipts();
-  
-  /* 注册监听事件 */
-  ipcMain.on('custom-event-player1', (event, board) => {
-    const result = script1(board);
-    event.reply('custom-event-player1-reply', result);
-  });
+  try {
+    const { script1, script2 } = getScipts();
 
-  ipcMain.on('custom-event-player2', (event, board) => {
-    const result = script2(board);
-    event.reply('custom-event-player2-reply', result);
-  });
+    /* 注册监听事件 */
+    ipcMain.on('custom-event-player1', (event, board) => {
+      const result = script1(board);
+      event.reply('custom-event-player1-reply', result);
+    });
+
+    ipcMain.on('custom-event-player2', (event, board) => {
+      const result = script2(board);
+      event.reply('custom-event-player2-reply', result);
+    });
+  } catch (e) {
+    dialog.showErrorBox('项目初始化失败', `${JSON.stringify(e.message)}`);
+  }
 
   /* 加载首页信息 */
   win.loadFile('./client/index.html');
